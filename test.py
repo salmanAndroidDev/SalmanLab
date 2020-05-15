fc69a82da713b05109d3a3559395ba82248865e2
Salman Barani <salmanAndroidDev@yahoo.com>
2020-05-16 04:23:14 +0430

    fix some stuff

--- test.py
+++ test.py
@@ -1,22 +1,6 @@
+from app.salMath import Math
 
-class Information:
-    
-    def __init__(self):
-        self.name = "salman"
-        self.__private_name = "Johnson"
+math = Math()
 
-    def get_private_name(self):
-        return self.__private_name
-        
-    def set_private_name(self,new_Name):
-        self.__private_name = new_Name
-        self.__display()
-
-    def __display(self):
-        print("This method shouldn't be displayed by the instances")
- 
-info = Information()
-info.set_private_name("Steve")
-
-
+print(math.add(1,2))
 
