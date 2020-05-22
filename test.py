e6de0371a29095804bcdf616eb790738ec201372
Salman Barani <salmanAndroidDev@yahoo.com>
2020-05-23 01:40:26 +0430

    solve programming langague project problem

--- test.py
+++ test.py
@@ -1,33 +1,95 @@
-def correctify_word(word):
+import math
 
-    for character in word:
-          if not character.isalnum():
-              word = word.replace(character,'')
-    return word
+variables = {}
+functions = ('add', 'sub', 'mul', 'div', 'pow', 'gcd', 'log')
 
+def is_input_value_valid(variable):
+    if variable.isdigit():
+        return True
+    elif variable.replace('_','').isalpha():
+        return True
+    return False
 
-def encode(text):
-    words =text.replace('\n',' ').split()
+def is_a_valid_function(value):
+        try:
+                # print('read-1')
+                function, second = value.split('(')
+                # print('read-2',second.split(')')[0])
+                val1, val2 = second.split(')')[0].split(',')
+                if(not function in functions):
+                    print('function not found')
+                    return False
+                else:
+                    print('val1: ', type(val1), ' val2: ', type(val2), ' operation: ', function)
+                    if is_input_value_valid(val1)  :
+                        var1 = variables.get(val1)
+                        
+                        if var1 == None and val1.isdigit() == False:
+                            print('variable error1')
+                            return False
+                    elif val1.isdigit() == False:
+                        print('val is not a number****')
+                        return False
+                    print('reads between')       
+                    if is_input_value_valid(val2) :
+                        var2 = variables.get(val2)
+                        print('read2')
+                        
+                        if var2 == None and val2.isdigit() == False:
+                            print('variable error2')
+                            return False
+                    elif val2.isdigit() == False:
+                        print('val is not a number====')
+                        return False
 
-    dic_words = {}
-    numbers = []
-    counter = 1
+                    return val1,val2,function
 
-    for word in words:
-        selected_word = word
-
-        if not word.isalnum():
-            selected_word = correctify_word(word)
-        
-        if selected_word == '':
-            continue
-
-        if not selected_word in dic_words:
-            dic_words[selected_word] = counter
-            numbers.append(counter)        
-            counter += 1
-        
-        else:
-            numbers.append(dic_words.get(selected_word))
+        except Exception as e:
+            print(e)
+            return False 
     
-    return dic_words, numbers
+
+output = ''
+while True:
+    command = input().replace(' ','').split(':=')   
+    
+
+    if('end' in command):
+        break
+
+    variable = command[0]
+
+    if len(command) == 1:
+            output = variables.get(variable)
+
+            if output == None:
+                output = "variable not found"
+                
+            print(output)    
+       
+    else:
+            value = command[1]
+            
+            result = is_a_valid_function(value)
+            
+            if  result != False:
+                val1, val2, operation = result
+
+                print('aval1: ', val1, ' val2: ', val2, operation)
+            
+            # ************************************************ variable assignment
+            
+            else:                   
+                if is_input_value_valid(variable) and not variable.isdigit():
+                    if is_input_value_valid(value):
+                        if value.isdigit():
+                            print('readx ', value)
+                            variables[variable] = value
+                        else:
+                            print('ready')
+                            variables[variable] = variables[value]
+                    else:
+                        print("val is not a number")
+                    
+                else:
+                    print('variable error')        
