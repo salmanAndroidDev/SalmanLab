5031fb7eda7ba472bf05ae6bfc5dadcfebda0ecf
Salman Barani <salmanAndroidDev@yahoo.com>
2020-06-17 10:06:44 +0430

    test automation on github

--- automation/test.py
+++ automation/test.py
@@ -1,16 +1,12 @@
 import pyautogui as pag
+import keyboard
 import time
 
-keys = pag.KEYBOARD_KEYS
 
-file_loc = pag.locateOnScreen('stock/one.png')
-download_loc = pag.locateOnScreen('stock/two.png')
-pic_loc = pag.locateOnScreen('stock/three.png')
+tile1 = 370
+tile2 = 451
+tile3 = 528
+til34 = 605
 
-# pag.click(file_loc)
-# time.sleep(4)
-pag.doubleClick(pic_loc)
-# pag.doubleClick(download_loc)
-# time.sleep(2)
-# pag.doubleClick(pic_loc)
-
+while keyboard.is_pressed('q') == False:
+    pass
