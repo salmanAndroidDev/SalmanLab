5f450e568198778b41f6fa62320ebfa90899f8c0
vagrant <vagrant@vagrant.vm>
2020-09-29 20:29:58 +0000

    add user profile and enable user and profile editing

--- Django/bookmarks/account/views.py
+++ Django/bookmarks/account/views.py
@@ -2,8 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-
-from .forms import LoginForm, UserRegisterationForm
+from .models import Profile
+from .forms import LoginForm, UserRegisterationForm, UserEditForm, ProfileEditForm
 
 def user_login(request):
     if request.method == 'POST':
@@ -40,6 +40,7 @@ def register(request):
             new_user.set_password(
                 user_form.cleaned_data['password'])
             new_user.save()
+            Profile.objects.create(user= new_user)
             return render(request,
                          'account/register_done.html',
                          {'new_user':new_user})
@@ -49,5 +50,23 @@ def register(request):
                 'account/register.html',
                 {'user_form': user_form})    
 
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user
+                                ,data= request.POST)
 
+        profile_form = ProfileEditForm(instance= request.user.profile,
+                                data= request.POST,
+                                files= request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+           user_form = UserEditForm(instance = request.user) 
+           profile_form = ProfileEditForm(instance=request.user.profile)
 
+    return render(request,
+                'account/edit.html',
+                {'user_form': user_form,
+                'profile_form': profile_form})         
