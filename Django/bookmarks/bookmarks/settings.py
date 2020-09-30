be76cf938cb00c80a3f9fe7e7794515264e1d650
vagrant <vagrant@vagrant.vm>
2020-09-30 08:11:17 +0000

    authenticate user against username and also email address

--- Django/bookmarks/bookmarks/settings.py
+++ Django/bookmarks/bookmarks/settings.py
@@ -102,6 +102,10 @@ USE_L10N = True
 
 USE_TZ = True
 
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'account.authentication.EmailAuthBackend',
+]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
