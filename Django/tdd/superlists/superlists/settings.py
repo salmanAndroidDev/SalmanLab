35ce1555877820319ef86d6c169f3beaec56e0d0
vagrant <vagrant@vagrant.vm>
2020-10-02 07:53:23 +0000

    set up practice project for bookmark

--- Django/tdd/superlists/superlists/settings.py
+++ Django/tdd/superlists/superlists/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'yd390lg0eq6ifek-!6t=)p5ifc^k-bm!jl_k&jaxu9hz)v8rzx'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mysite.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
